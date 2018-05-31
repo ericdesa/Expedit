22b4d83bd622714b31f4d236012f4be307d69228
Eric <eric@frianbiz.com>
2018-05-31 16:00:31 +0200

    chore(example): add comments

--- Sample-iOS/Expedit-iOS/GUI/MainNavigationController.swift
+++ Sample-iOS/Expedit-iOS/GUI/MainNavigationController.swift
@@ -6,15 +6,19 @@ class MainNavigationController: UINavigationController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // the routes are handled here
+        // you are responsible to present the associated VC
         RouteManager.addObserver { (route) -> Bool in
             
             var isRouteOpened = false
             
+            // some routes build the VC for you ...
             if let vc = route.viewController() {
                 self.pushViewController(vc, animated: true)
                 isRouteOpened = true
             }
             
+            // ... but you can also throw other actions, like a share one
             else if route is RouteManual {
                 self.promptRouteManual()
                 isRouteOpened = true
