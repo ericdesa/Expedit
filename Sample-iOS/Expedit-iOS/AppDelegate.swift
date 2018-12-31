12247295147bbd86779000886a92038d978962a9
Eric <ede@frianbiz.com>
2018-12-31 16:26:33 +0100

    chore(demo): upgrade to swift 4.2

--- Sample-iOS/Expedit-iOS/AppDelegate.swift
+++ Sample-iOS/Expedit-iOS/AppDelegate.swift
@@ -7,12 +7,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         RouteManager.setDebugMode(.verbose)
         return true
     }
     
-    public func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+    public func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
         return RouteManager.open(url: url)
     }
 }
